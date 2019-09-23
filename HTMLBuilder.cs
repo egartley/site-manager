b9584a54621444283cfd3a9968f2ba970eb66c41
Evan Gartley <egartley@users.noreply.github.com>
2019-09-22 20:43:45 -0400

    Update HTMLBuilder.cs
    Modified for new design

--- HTMLBuilder.cs
+++ HTMLBuilder.cs
@@ -10,51 +10,33 @@
 
         public static string GetNavigationBarHTML(string pageurl)
         {
-            // lazy way to handle determining what nav item is active if any at all
-            // dependent upon the inputted nav code, doesn't work without it being specifically configured
-            // should probably be more dynamic in the future, but it works now, so...
             if (pageurl.Equals("/"))
             {
                 pageurl = "home";
             }
             else
             {
-                // lazy fix (again)
                 pageurl += "/";
-                // /views/conspiracies/
                 pageurl = pageurl.Substring(1);
-                // views/conspiracies
                 pageurl = pageurl.Substring(0, pageurl.IndexOf("/"));
-                // pageurl is now "views" (hopefully)
             }
-            string anchor = "class=\"nav-item ";
-            string modifiedcode = CoreManager.GetModuleByTag("navbar").Code;
-            string returncode = modifiedcode.Substring(0, modifiedcode.IndexOf(anchor));
-            string originalcode = modifiedcode;
-
-            if (originalcode.IndexOf(pageurl) == -1)
+            string lookfor = $"page=\"{pageurl}";
+            string original = CoreManager.GetModuleByTag("navbar").Code;
+            if (!original.Contains(lookfor))
             {
-                // don't bother continuing, won't be in the nav bar
-                return originalcode;
+                return original.Replace("page=\"", "class=\"hitbox\" page=\"");
             }
-
-            while (modifiedcode.IndexOf(anchor) != -1)
+            else
             {
-                modifiedcode = modifiedcode.Substring(modifiedcode.IndexOf(anchor) + anchor.Length);
-                string titleClass = modifiedcode.Substring(0, modifiedcode.IndexOf("\">")).ToLower();
-                if (titleClass == pageurl)
-                {
-                    returncode = originalcode.Replace(anchor + titleClass, anchor + "active");
-                    break;
-                }
+                return original.Replace(lookfor, "class=\"hitbox active\" page=\"");
             }
-            return returncode;
         }
 
         public static string GetFullPageHTML(ManagedWebPage page)
         {
             Debug.Out($"Generating full HTML for \"{page.Title}\" ({page.RelativeURL})", "HTML BUILDER");
-            return "<!DOCTYPE html>\n<head>\n" + GlobalString.HTML_BUILDER_SIGNATURE + "\n<title>" + page.Title + " - egartley.net</title>\n" + CoreManager.GetModuleByTag("header").Code + "\n" + page.AdditionalHeaderHTML + "\n</head>\n<body>\n<div class=\"root-container\">\n<div class=\"container navbar-container\">\n" + GetNavigationBarHTML(page.RelativeURL.ToLower()) + "\n</div>\n<div class=\"container-container\">\n<div class=\"container pagecontent-container\">\n<div class=\"page-title\">\n<span>" + page.Title + "</span>\n</div\n><div class=\"page-meta\">\n<span class=\"secondary-text\">Last updated " + page.GetLastUpdatedAsString() + "</span>\n</div>\n<div class=\"page-content\">\n" + page.ContentHTML + "\n</div>\n</div>\n<div class=\"container sidebar-container\">\n" + CoreManager.GetModuleByTag("sidebar").Code + "\n</div>\n</div>\n</div>\n<div class=\"container footer-container\">\n" + CoreManager.GetModuleByTag("footer").Code + "\n</div>\n</body>\n</html>";
+            // return "<!DOCTYPE html>\n<head>\n" + GlobalString.HTML_BUILDER_SIGNATURE + "\n<title>" + page.Title + " - egartley.net</title>\n" + CoreManager.GetModuleByTag("header").Code + "\n" + page.AdditionalHeaderHTML + "\n</head>\n<body>\n<div class=\"root-container\">\n<div class=\"container navbar-container\">\n" + GetNavigationBarHTML(page.RelativeURL.ToLower()) + "\n</div>\n<div class=\"container-container\">\n<div class=\"container pagecontent-container\">\n<div class=\"page-title\">\n<span>" + page.Title + "</span>\n</div\n><div class=\"page-meta\">\n<span class=\"secondary-text\">Last updated " + page.GetLastUpdatedAsString() + "</span>\n</div>\n<div class=\"page-content\">\n" + page.ContentHTML + "\n</div>\n</div>\n<div class=\"container sidebar-container\">\n" + CoreManager.GetModuleByTag("sidebar").Code + "\n</div>\n</div>\n</div>\n<div class=\"container footer-container\">\n" + CoreManager.GetModuleByTag("footer").Code + "\n</div>\n</body>\n</html>";
+            return "<!DOCTYPE html>\n<head>\n" + GlobalString.HTML_BUILDER_SIGNATURE + "\n<title>" + page.Title + " - egartley.net</title>\n" + CoreManager.GetModuleByTag("header").Code + "\n" + page.AdditionalHeaderHTML + "\n</head>\n<body><div class=\"root-container\"><div class=\"navigation-bar-container\">" + GetNavigationBarHTML(page.RelativeURL.ToLower()) + "</div><div class=\"content-container\"><div class=\"page-container content-card\"><div class=\"page-content-container\"><span class=\"page-title\">" + page.Title + "</span><span class=\"gradient-spacer\"></span><span class=\"page-meta\">Last updated " + page.GetLastUpdatedAsString() + "</span><div class=\"content\">" + page.ContentHTML + "</div></div></div><div class=\"sidebar-container content-card\">" + CoreManager.GetModuleByTag("sidebar").Code + "</div></div><div class=\"footer-container\">" + CoreManager.GetModuleByTag("footer").Code + "</div></div></body></html>";
         }
     }
 }
